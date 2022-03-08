d2c8ce0265052b913c1be907a3e8ddffeb93ea3f
Hafizz <81644042+hafizzganss678@users.noreply.github.com>
2022-03-08 17:45:48 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -18,9 +18,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.owner = ['6285892842367']
+global.premium = ['6285892842367']
+global.packname = 'Hafizz Multi'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
